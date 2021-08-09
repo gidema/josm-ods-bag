629a6bc52e3cff1580dc3c668b8ab6e5b3534805
SanderH <sander.hoogwerf@xs4all.nl>
2021-08-09 19:34:56 +0100

    Relocate JOSM preference location

--- src/main/java/org/openstreetmap/josm/plugins/ods/bag/BagProperties.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/bag/BagProperties.java
@@ -6,9 +6,9 @@ import org.openstreetmap.josm.tools.I18n;
 
 public final class BagProperties {
 
-    public static final StringProperty WFS_URL = new StringProperty("nl.bag.pdok.bag.wfs.url", "https://geodata.nationaalgeoregister.nl/bag/wfs/v1_1");
-    public static final IntegerProperty WFS_INIT_TIMEOUT = new IntegerProperty("nl.bag.pdok.bag.wfs.init_timeout", 1000);
-    public static final IntegerProperty WFS_DATA_TIMEOUT = new IntegerProperty("nl.bag.pdok.bag.wfs.data_timeout", 10000);
+    public static final StringProperty WFS_URL = new StringProperty("nl.bag.pdok.wfs.url", "https://geodata.nationaalgeoregister.nl/bag/wfs/v1_1");
+    public static final IntegerProperty WFS_INIT_TIMEOUT = new IntegerProperty("nl.bag.pdok.wfs.init_timeout", 1000);
+    public static final IntegerProperty WFS_DATA_TIMEOUT = new IntegerProperty("nl.bag.pdok.wfs.data_timeout", 10000);
 
   private BagProperties() {
     // Private constructor to avoid instantiation
